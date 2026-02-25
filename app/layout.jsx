c93b7d7e48fb6cf2a55c2798a80ba0496e893002
mostafaatefaly-lgtm <mostafaatefaly@gmail.com>
2026-02-25 18:07:12 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,19 @@
 
+// app/layout.jsx
+
+export const metadata = {
+  title: "حجز قاعة الاجتماعات",
+  description: "نظام حجز قاعة الاجتماعات"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ar" dir="rtl">
+      <body>{children}</body>
+    </html>
+  );
+}
+
 // app/page.jsx
 'use client';
 import { useEffect, useState } from 'react';
