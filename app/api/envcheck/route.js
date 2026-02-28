fd918164fdccbafa31452dccfbe8253122ed93e4
mostafaatefaly-lgtm <mostafaatefaly@gmail.com>
2026-02-28 17:24:58 +0300

    Create route.js

---
+++ app/api/envcheck/route.js
@@ -0,0 +1,12 @@
+
+export const dynamic = 'force-dynamic';
+
+export async function GET() {
+  const vars = {
+    UPSTASH_REDIS_REST_URL: !!process.env.UPSTASH_REDIS_REST_URL,
+    UPSTASH_REDIS_REST_TOKEN: !!process.env.UPSTASH_REDIS_REST_TOKEN,
+    KV_REST_API_URL: !!process.env.KV_REST_API_URL,
+    KV_REST_API_TOKEN: !!process.env.KV_REST_API_TOKEN,
+  };
+  return Response.json({ ok: true, present: vars });
+}
